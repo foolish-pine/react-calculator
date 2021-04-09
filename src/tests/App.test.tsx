5a28e7cd33822cc710fc613ccee5d58a6b18034e
Takuya Matsuda <matsuda.takuya41@gmail.com>
2021-04-09 19:20:00 +0900

    [add] Appコンポーネントのテスト追加

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -5,72 +5,192 @@ import App from "../components/App";
 import userEvent from "@testing-library/user-event";
 
 describe("Appコンポーネント", () => {
-  test("0のキーをクリックすると、0が表示される", () => {
+  test("初期状態では数式表示欄には何も表示されない", () => {
     render(<App />);
-    userEvent.click(screen.getByText("0"));
-    expect(screen.getByDisplayValue("0")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
   });
-  test("1のキーをクリックすると、1が表示される", () => {
+  test("「1」ボタンをクリックすると、表示されている数式の末尾に1が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("1"));
+    userEvent.click(screen.getByRole("button", { name: "1" }));
     expect(screen.getByDisplayValue("1")).toBeInTheDocument();
   });
-  test("2のキーをクリックすると、2が表示される", () => {
+  test("「2」ボタンをクリックすると、表示されている数式の末尾に2が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("2"));
+    userEvent.click(screen.getByRole("button", { name: "2" }));
     expect(screen.getByDisplayValue("2")).toBeInTheDocument();
   });
-  test("3のキーをクリックすると、3が表示される", () => {
+  test("「3」ボタンをクリックすると、表示されている数式の末尾に3が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("3"));
+    userEvent.click(screen.getByRole("button", { name: "3" }));
     expect(screen.getByDisplayValue("3")).toBeInTheDocument();
   });
-  test("4のキーをクリックすると、4が表示される", () => {
+  test("「4」ボタンをクリックすると、表示されている数式の末尾に4が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("4"));
+    userEvent.click(screen.getByRole("button", { name: "4" }));
     expect(screen.getByDisplayValue("4")).toBeInTheDocument();
   });
-  test("5のキーをクリックすると、5が表示される", () => {
+  test("「5」ボタンをクリックすると、表示されている数式の末尾に5が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("5"));
+    userEvent.click(screen.getByRole("button", { name: "5" }));
     expect(screen.getByDisplayValue("5")).toBeInTheDocument();
   });
-  test("6のキーをクリックすると、6が表示される", () => {
+  test("「6」ボタンをクリックすると、表示されている数式の末尾に6が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("6"));
+    userEvent.click(screen.getByRole("button", { name: "6" }));
     expect(screen.getByDisplayValue("6")).toBeInTheDocument();
   });
-  test("7のキーをクリックすると、7が表示される", () => {
+  test("「7」ボタンをクリックすると、表示されている数式の末尾に7が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("7"));
+    userEvent.click(screen.getByRole("button", { name: "7" }));
     expect(screen.getByDisplayValue("7")).toBeInTheDocument();
   });
-  test("8のキーをクリックすると、8が表示される", () => {
+  test("「8」ボタンをクリックすると、表示されている数式の末尾に8が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("8"));
+    userEvent.click(screen.getByRole("button", { name: "8" }));
     expect(screen.getByDisplayValue("8")).toBeInTheDocument();
   });
-  test("9のキーをクリックすると、9が表示される", () => {
+  test("「9」ボタンをクリックすると、表示されている数式の末尾に9が追加される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("9"));
+    userEvent.click(screen.getByRole("button", { name: "9" }));
     expect(screen.getByDisplayValue("9")).toBeInTheDocument();
   });
-  test("表示されている数式の最後の数字が0の場合、続けて数字をクリックしても表示は変化しない", () => {
+  test("「AC」ボタンをクリックすると、数式がクリアされる", () => {
     render(<App />);
-    userEvent.click(screen.getByText("5"));
-    userEvent.click(screen.getByText("+"));
-    userEvent.click(screen.getByText("0"));
-    userEvent.click(screen.getByText("1"));
-    expect(screen.getByDisplayValue("5+0")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "3" }));
+    expect(screen.getByDisplayValue("23")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "AC" }));
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
   });
-  test("最初に0以外の数字をクリックしたあと数字をクリックすると、その数字が表示の末尾に追加される", () => {
+  test("「Del」ボタンをクリックすると、数式の末尾の1文字が削除される", () => {
     render(<App />);
-    userEvent.click(screen.getByText("1"));
-    userEvent.click(screen.getByText("0"));
-    userEvent.click(screen.getByText("1"));
-    userEvent.click(screen.getByText("4"));
-    expect(screen.getByDisplayValue("1014")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "3" }));
+    expect(screen.getByDisplayValue("23")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "Del" }));
+    expect(screen.getByDisplayValue("2")).toBeInTheDocument();
   });
+  test("「+」ボタンをクリックすると、数式の末尾に+が追加される", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    expect(screen.getByDisplayValue("2+")).toBeInTheDocument();
+  });
+  test("「-」ボタンをクリックすると、数式の末尾に-が追加される", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    expect(screen.getByDisplayValue("2-")).toBeInTheDocument();
+  });
+  test("「×」ボタンをクリックすると、数式の末尾に×が追加される", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2×")).toBeInTheDocument();
+  });
+  test("「÷」ボタンをクリックすると、数式の末尾に÷が追加される", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2÷")).toBeInTheDocument();
+  });
+  test.todo(
+    "数式の末尾が.のとき、演算子ボタンをクリックしても表示は変化しない"
+  );
+  test("数式が空のとき、演算子ボタンをクリックしても表示は変化しない", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
+  });
+  test("数式の末尾が+または-のとき、演算子ボタンをクリックしても表示は変化しない", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    expect(screen.getByDisplayValue("2+")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    expect(screen.getByDisplayValue("2+")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2+")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2+")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "AC" }));
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    expect(screen.getByDisplayValue("2-")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    expect(screen.getByDisplayValue("2-")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2-")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2-")).toBeInTheDocument();
+  });
+  test("数式の末尾が×または÷のとき「-」ボタンをクリックすると数式の末尾に-が追加されるが、それ以外の演算子ボタンをクリックしても表示は変化しない", () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2×")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    expect(screen.getByDisplayValue("2×-")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "AC" }));
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2÷")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "-" }));
+    expect(screen.getByDisplayValue("2÷-")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "AC" }));
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2×")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    expect(screen.getByDisplayValue("2×")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2×")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2×")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "AC" }));
+    userEvent.click(screen.getByRole("button", { name: "2" }));
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2÷")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+    expect(screen.getByDisplayValue("2÷")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "×" }));
+    expect(screen.getByDisplayValue("2÷")).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "÷" }));
+    expect(screen.getByDisplayValue("2÷")).toBeInTheDocument();
+  });
+  test.todo("「.」ボタンをクリックすると、数式の末尾に.が追加される");
+  test.todo("数式が空のとき、「.」ボタンをクリックしても表示は変化しない");
+  test.todo(
+    "数式の末尾が小数点または演算子のとき、「.」ボタンをクリックしても表示は変化しない"
+  );
+  test.todo(
+    "数式の最後の数字がすでに小数点を含む場合、「.」ボタンをクリックしても表示は変化しない"
+  );
+  // test("表示されている数式の最後の数字が0の場合、続けて数字をクリックしても表示は変化しない", () => {
+  //   render(<App />);
+  //   userEvent.click(screen.getByText("5"));
+  //   userEvent.click(screen.getByText("+"));
+  //   userEvent.click(screen.getByText("0"));
+  //   userEvent.click(screen.getByText("1"));
+  //   expect(screen.getByDisplayValue("5+0")).toBeInTheDocument();
+  // });
+  // test("最初に0以外の数字をクリックしたあと数字をクリックすると、その数字が表示の末尾に追加される", () => {
+  //   render(<App />);
+  //   userEvent.click(screen.getByText("1"));
+  //   userEvent.click(screen.getByText("0"));
+  //   userEvent.click(screen.getByText("1"));
+  //   userEvent.click(screen.getByText("4"));
+  //   expect(screen.getByDisplayValue("1014")).toBeInTheDocument();
+  // });
   test.todo("何も表示されていない状態で.をクリックしても表示は変化しない");
   test.todo(
     "数字がクリックされたあとに.をクリックすると、表示の末尾に.が追加される"
